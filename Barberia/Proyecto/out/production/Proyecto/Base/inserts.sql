4f962961244bd3879391e62df71af6cd1223d04f
reader2330 <fernando@MacBook-Pro-de-Fernando.local>
2017-11-25 09:37:19 -0600

    Proyect

--- Barberia/Proyecto/out/production/Proyecto/Base/inserts.sql
+++
@@ -1,5 +0,0 @@
-insert into producto values(2,"Acondicionador","Acondicionar de para cabello",300);
-insert into producto values(3,"Crema de afeitar","Crema de afeitar para barba",150);
-insert into producto values(4,"Locion","Para despues de Afeitarse",200);
-
-select * from producto;
--- Barberia/Proyecto/out/production/Proyecto/Base/insproducto.sql
+++
@@ -1,7 +0,0 @@
-use proyecto;
-
-insert into producto values(2,"Acondicionador","Acondicionar de para cabello",300);
-insert into producto values(3,"Crema de afeitar","Crema de afeitar para barba",150);
-insert into producto values(4,"Locion","Para despues de Afeitarse",200);
-
-select * from producto;
